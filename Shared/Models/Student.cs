56a3f0fb6a7e791063dd3f540d2df3cb3e38e139
Qadrina <najlaqadrina@gmail.com>
2022-10-01 19:22:06 +0700

    add css file, logo, fix titles on index

--- Shared/Models/Student.cs
+++ Shared/Models/Student.cs
@@ -9,5 +9,7 @@ namespace University_App.Shared.Models
         public string? Student_Name { get; set; }
         public DateTime? Birth_Date { get; set; }
         public string? Faculty_Id { get; set; }
+        public string? Nationality { get; set; }
+        public int ? Class_Year { get; set; }
     }
 }
